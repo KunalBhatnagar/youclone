e7a53608bce7809a89eb70073d9687d57777e658
Kunal Bhatnagar <kbhatnagar17.kb@gmail.com>
2025-01-21 16:33:23 -0500

    utils code discussion

---
+++ src/utils/async-Handler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) =>{
+    (req, res, next) =>{
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+
+export {asyncHandler}
+
+
+
+// const asynchandler = (func) => {() => {}} which can be written as = (func) => () => {}
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error){
+//         res.this.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
