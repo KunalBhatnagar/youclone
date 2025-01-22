347a2a87e5fdaf54d14a28a74ccab9803e0e6ee8
Kunal Bhatnagar <kbhatnagar17.kb@gmail.com>
2025-01-22 17:15:36 -0500

    all set up configure

--- src/app.js
+++ src/app.js
@@ -14,4 +14,13 @@ app.use(express.urlencoded({extended:true, limit:'16kb'}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+// routes import
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+
+// now the users route below become prefix and when it transfer to user route then whichever next route we call it will used as /users/next_route
+app.use("/api/v1/users", userRouter)
+
 export {app}
