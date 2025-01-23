42c3d148d4b8a51505b824f01040192b9dd4b32b
Kunal Bhatnagar <kbhatnagar17.kb@gmail.com>
2025-01-23 16:15:04 -0500

    register controller bugs fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -25,7 +25,7 @@ const registerUser = asyncHandler( async (req, res) =>{
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [ { username }, { email } ]
     })
 
@@ -34,7 +34,11 @@ const registerUser = asyncHandler( async (req, res) =>{
     }
     // . files come with multer and ? represent optionally
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalpath = req.files?.coverImage[0]?.path;
+   // const coverImageLocalpath = req.files?.coverImage[0]?.path;
+   let coverImageLocalpath;
+   if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0){
+    coverImageLocalpath =req.files.coverImage[0].path;
+   }
     
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar files is required")
@@ -42,7 +46,7 @@ const registerUser = asyncHandler( async (req, res) =>{
     
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalpath)
-
+ 
     if(!avatar){
         throw new ApiError(400, "Avatar files is required")
     }
@@ -64,4 +68,4 @@ const registerUser = asyncHandler( async (req, res) =>{
     )
 })
 
-export {registerUser}
+export {registerUser}  
