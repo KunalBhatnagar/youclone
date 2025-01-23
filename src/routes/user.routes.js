6fd6bd62063a913bee32d8ce6ef8a3a82738d8c8
Kunal Bhatnagar <kbhatnagar17.kb@gmail.com>
2025-01-23 15:20:22 -0500

    add register controller and bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router()
 
-
-router.route("/register").post(registerUser)
+// middleware is used just before (upload is imported as middleware) it redirect to registerUser route
+router.route("/register").post(upload.fields([
+    {
+        name: "avatar",
+        maxCount:1
+    },
+    {
+        name: "coverImage",
+        maxCount: 1
+    }
+]),registerUser)
 
 export default router
